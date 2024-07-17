d2eba086d03393bee682177f3b4f3849fdd7778d
J0SH1X <aljoshua.hell@gmail.com>
2024-07-17 18:38:05 +0000

    DEV: inital groundwork for single row keypads

--- lk/dev/keys/gpio_keypad.c
+++ lk/dev/keys/gpio_keypad.c
@@ -72,8 +72,11 @@ static void check_output(struct gpio_kp *kp, int out, int polarity)
 	int in;
 	int gpio;
 	int changed = 0;
-
+if (kpinfo->noutputs > 0){
 	key_index = out * kpinfo->ninputs;
+   }else {
+	//single bank code here
+   }
 	for (in = 0; in < kpinfo->ninputs; in++, key_index++) {
 		gpio = kpinfo->input_gpios[in];
 		changed = 0;
@@ -91,11 +94,15 @@ static void check_output(struct gpio_kp *kp, int out, int polarity)
 	}
 
 	/* sets up the right state for the next poll cycle */
+	if (kpinfo->noutputs > 0){
 	gpio = kpinfo->output_gpios[out];
 	if (kpinfo->flags & GPIOKPF_DRIVE_INACTIVE)
 		gpio_set(gpio, !polarity);
 	else
 		gpio_config(gpio, GPIO_INPUT);
+	}else {
+		//single bank stuff here
+	}
 }
 
 static enum handler_return
@@ -104,20 +111,23 @@ gpio_keypad_timer_func(struct timer *timer, time_t now, void *arg)
 	struct gpio_kp *kp = keypad;
 	struct gpio_keypad_info *kpinfo = kp->keypad_info;
 	int polarity = !!(kpinfo->flags & GPIOKPF_ACTIVE_HIGH);
-	int out;
+	int out = 0;
 	int gpio;
-
-	out = kp->current_output;
-	if (out == kpinfo->noutputs) {
+	if (kpinfo->noutputs > 0){
+	   out = kp->current_output;
+	if (out == kpinfo->noutputs && kpinfo->noutputs > 0) {
 		out = 0;
 		kp->some_keys_pressed = 0;
 	} else {
 		check_output(kp, out, polarity);
 		out++;
 	}
-
-	kp->current_output = out;
-	if (out < kpinfo->noutputs) {
+	}else {
+		//single bank stufferino here !!!
+	}
+	if (kpinfo->noutputs > 0)
+	   kp->current_output = out;
+	if (out < kpinfo->noutputs && kpinfo->noutputs > 0) {
 		gpio = kpinfo->output_gpios[out];
 		if (kpinfo->flags & GPIOKPF_DRIVE_INACTIVE)
 			gpio_set(gpio, polarity);
@@ -126,6 +136,8 @@ gpio_keypad_timer_func(struct timer *timer, time_t now, void *arg)
 		timer_set_oneshot(timer, kpinfo->settle_time,
 				  gpio_keypad_timer_func, NULL);
 		goto done;
+	}else {
+		//single bank code here !!!
 	}
 
 	if (/*!kp->use_irq*/ 1 || kp->some_keys_pressed) {
@@ -160,9 +172,14 @@ void gpio_keypad_init(struct gpio_keypad_info *kpinfo)
 	int i;
 	int len;
 
-	ASSERT(kpinfo->keymap && kpinfo->input_gpios && kpinfo->output_gpios);
-	key_count = kpinfo->ninputs * kpinfo->noutputs;
-
+	
+	if (kpinfo->output_gpios){
+		ASSERT(kpinfo->keymap && kpinfo->input_gpios && kpinfo->output_gpios);
+		key_count = kpinfo->ninputs * kpinfo->noutputs;
+	}else {
+		ASSERT(kpinfo->keymap && kpinfo->input_gpios);
+		key_count = kpinfo->ninputs;
+	}
 	len = sizeof(struct gpio_kp) + (sizeof(unsigned long) *
 					BITMAP_NUM_WORDS(key_count));
 	keypad = malloc(len);
@@ -171,17 +188,18 @@ void gpio_keypad_init(struct gpio_keypad_info *kpinfo)
 	memset(keypad, 0, len);
 	keypad->keypad_info = kpinfo;
 
-	output_val = (!!(kpinfo->flags & GPIOKPF_ACTIVE_HIGH)) ^
-		     (!!(kpinfo->flags & GPIOKPF_DRIVE_INACTIVE));
+	if (kpinfo->output_gpios){
+	output_val = (!!(kpinfo->flags & GPIOKPF_ACTIVE_HIGH)) ^ (!!(kpinfo->flags & GPIOKPF_DRIVE_INACTIVE));
 	output_cfg = kpinfo->flags & GPIOKPF_DRIVE_INACTIVE ? GPIO_OUTPUT : 0;
 	for (i = 0; i < kpinfo->noutputs; i++) {
 		gpio_set(kpinfo->output_gpios[i], output_val);
 		gpio_config(kpinfo->output_gpios[i], output_cfg);
 	}
+	}
 	for (i = 0; i < kpinfo->ninputs; i++)
 		gpio_config(kpinfo->input_gpios[i], GPIO_INPUT);
-
-	keypad->current_output = kpinfo->noutputs;
+	if (kpinfo->noutputs > 0)
+	   keypad->current_output = kpinfo->noutputs;
 
 	event_init(&keypad->full_scan, false, EVENT_FLAG_AUTOUNSIGNAL);
 	timer_initialize(&keypad->timer);
