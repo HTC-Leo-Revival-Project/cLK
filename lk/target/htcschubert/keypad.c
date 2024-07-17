4163f91e5173eae35e36fdf6cd35b9ad4c571b46
J0SH1X <aljoshua.hell@gmail.com>
2024-07-17 18:38:40 +0000

    HTC Schubert: start fixing keys

--- lk/target/htcschubert/keypad.c
+++ lk/target/htcschubert/keypad.c
@@ -3,26 +3,23 @@
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof((x)[0]))
 
-static unsigned int htcleo_row_gpios[] = { 33, 32, 31 };
-static unsigned int htcleo_col_gpios[] = { 42, 41, 40 };
+static unsigned int htcleo_col_gpios[] = { 90, 94, 41, 42 };
 
-#define KEYMAP_INDEX(row, col) ((row)*ARRAY_SIZE(htcleo_col_gpios) + (col))
 
-static const unsigned short htcleo_keymap[ARRAY_SIZE(htcleo_col_gpios) * ARRAY_SIZE(htcleo_row_gpios)] = {
-	[KEYMAP_INDEX(0, 0)] = KEY_VOLUMEUP,	// Volume Up
-	[KEYMAP_INDEX(0, 1)] = KEY_VOLUMEDOWN,	// Volume Down
-	[KEYMAP_INDEX(1, 0)] = KEY_SOFT1,		// Windows Button
-	[KEYMAP_INDEX(1, 1)] = KEY_SEND,		// Dial Button
-	[KEYMAP_INDEX(1, 2)] = KEY_CLEAR,		// Hangup Button
-	[KEYMAP_INDEX(2, 0)] = KEY_BACK,		// Back Button
-	[KEYMAP_INDEX(2, 1)] = KEY_HOME,		// Home Button
+#define KEYMAP_INDEX(col) (col)
+
+static const unsigned short htcleo_keymap[ARRAY_SIZE(htcleo_col_gpios)] = {
+    [KEYMAP_INDEX(0)] = KEY_SEND,    // Volume Up
+    [KEYMAP_INDEX(1)] = KEY_VOLUMEUP,  // Volume Down
+    [KEYMAP_INDEX(2)] = KEY_VOLUMEDOWN,       // Windows Button
+    [KEYMAP_INDEX(3)] = KEY_CLEAR,        // Dial Button
 };
 
 static struct gpio_keypad_info htcleo_keypad_info = {
 	.keymap		= htcleo_keymap,
-	.output_gpios	= htcleo_row_gpios,
+	.output_gpios	= NULL,
 	.input_gpios	= htcleo_col_gpios,
-	.noutputs	= ARRAY_SIZE(htcleo_row_gpios),
+	.noutputs	= 0,
 	.ninputs	= ARRAY_SIZE(htcleo_col_gpios),
 	.settle_time	= 40 /* msec */,
 	.poll_time	= 20 /* msec */,
