a35a44beac0ae1fecbd90ff923a07035d623f4b8
J0SH1X <aljoshua.hell@gmail.com>
2024-07-21 23:54:15 +0000

    HTCSCHUBERT: fix keys

--- lk/target/htcschubert/keypad.c
+++ lk/target/htcschubert/keypad.c
@@ -1,32 +1,72 @@
 #include <dev/keys.h>
-#include <dev/gpio_keypad.h>
+#include <kernel/event.h>
+#include <kernel/timer.h>
+#include <dev/gpio.h>
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof((x)[0]))
 
-static unsigned int htcleo_col_gpios[] = { 90, 94, 41, 42 };
+event_t keyEvent;
 
-
-#define KEYMAP_INDEX(col) (col)
-
-static const unsigned short htcleo_keymap[ARRAY_SIZE(htcleo_col_gpios)] = {
-    [KEYMAP_INDEX(0)] = KEY_SEND,    // Volume Up
-    [KEYMAP_INDEX(1)] = KEY_VOLUMEUP,  // Volume Down
-    [KEYMAP_INDEX(2)] = KEY_VOLUMEDOWN,       // Windows Button
-    [KEYMAP_INDEX(3)] = KEY_CLEAR,        // Dial Button
+enum SchubertButtons {
+    VOLUMEDOWN,
+    VOLUMEUP,
+    CAMERA,
+    POWER
 };
 
-static struct gpio_keypad_info htcleo_keypad_info = {
-	.keymap		= htcleo_keymap,
-	.output_gpios	= NULL,
-	.input_gpios	= htcleo_col_gpios,
-	.noutputs	= 0,
-	.ninputs	= ARRAY_SIZE(htcleo_col_gpios),
-	.settle_time	= 40 /* msec */,
-	.poll_time	= 20 /* msec */,
-	.flags		= GPIOKPF_DRIVE_INACTIVE,
+struct KeyStruct {
+    enum SchubertButtons buttonname; // Enum for the button name
+    unsigned keycode;                // Key code
+    unsigned gpio;                   // Corresponding GPIO key
 };
 
-void keypad_init(void)
-{
-	gpio_keypad_init(&htcleo_keypad_info);
+struct KeyStruct htcschubert_keymap[4] = {
+    {CAMERA, KEY_SEND, 90},
+    {VOLUMEUP, KEY_VOLUMEUP, 94},
+    {VOLUMEDOWN, KEY_VOLUMEDOWN, 41},
+    {POWER, KEY_CLEAR, 42}
+};
+
+// Static array to store previous states of keys
+static int previous_states[4] = {1, 1, 1, 1}; // Initialize to 1 (keys not pressed)
+
+// Function to check the state of the keys and post events
+void check_keys(void) {
+    for (int i = 0; i < 4; i++) {
+        int gpio_state = gpio_get(htcschubert_keymap[i].gpio);
+
+        // Adjust for pull-down configuration:
+        // 0 indicates a key press
+        // 1 indicates a key release
+        int key_pressed = (gpio_state == 0);
+
+        // Only post an event if the state has changed
+        if (key_pressed != previous_states[i]) {
+            keys_post_event(htcschubert_keymap[i].keycode, key_pressed);
+            previous_states[i] = key_pressed; // Update the previous state
+        }
+    }
+}
+
+static enum handler_return gpio_keypad_timer_func(struct timer *timer, time_t now, void *arg) {
+    // Check the keys using the global htcschubert_keymap array
+    check_keys();
+
+    // Reschedule the timer
+    event_signal(&keyEvent, false);
+    timer_set_oneshot(timer, 100, gpio_keypad_timer_func, NULL);
+
+    return INT_RESCHEDULE;
+}
+
+void gpio_keys_init(void) {
+    static timer_t keypad_timer;
+    event_init(&keyEvent, false, EVENT_FLAG_AUTOUNSIGNAL);
+    timer_initialize(&keypad_timer);
+    timer_set_oneshot(&keypad_timer, 100, gpio_keypad_timer_func, NULL);
+    event_wait(&keyEvent);
+}
+
+void keypad_init(void) {
+    gpio_keys_init();
 }
